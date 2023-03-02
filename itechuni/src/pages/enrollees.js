36673b6cc9709f8191750635d42a37875bf50cbc
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-03-02 22:18:44 +0800

    Semi-finished Styles on Enrollees

--- itechuni/src/pages/enrollees.js
+++ itechuni/src/pages/enrollees.js
@@ -1,20 +1,13 @@
-import Navbar from '../Navbar';
 import React from 'react';
-  
+import Navbar from '../Navbar';
+import TableRecord from './components/enrollees/table';
+
 const Enrollees = () => {
   return (
     <>
     <Navbar></Navbar>
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
-      }}
-    >
-      <h1>Enrollees</h1>
-    </div>
+
+      <TableRecord></TableRecord>
     </>
   );
 };
