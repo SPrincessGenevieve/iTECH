e6c336c89050ee106d7af9ad6f1401c9adfa45e2
Sagrado, Princess Genevieve <xhanhime@gmai.com>
2023-03-02 21:11:05 +0800

    Added Forgot Password Button Props

---
+++ itechuni/src/login/component/ForgotPassBtn.js
@@ -0,0 +1,17 @@
+
+import './css/button.css'
+
+function ForgotPassBtn() {
+
+    return (
+      <body > 
+          <button className='buttonFP'>Forgot Password?</button>
+    </body>
+
+      );
+    }
+
+
+
+
+export default ForgotPassBtn;
