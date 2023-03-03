b92248d9bb19202b4049007c340f7fc2d70b8bfc
Bagongon , Trisha <bagongontrisha@gmail.com>
2023-03-03 23:28:26 +0800

    Dashboard  Design

---
+++ itechuni/srcCopy/Dashboard.js
@@ -0,0 +1,69 @@
+import Navbar from '../Navbar';
+import React from 'react';
+import './../pages/Dashboard.css'
+
+function Dashboard() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <div className='grey-container'>
+    <div className='Box'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>Total Students Enrolled</h3>
+          </div>
+    </div>
+    <div className='Box1'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>1st YEAR</h3>
+          </div>
+    </div>
+    <div className='Box2'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>2ND YEAR</h3>
+          </div>
+    </div>
+    <div className='Box3'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>3RD YEAR</h3>
+          </div>
+    <div className='Box4'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>4TH YEAR</h3>
+          </div>
+    </div>
+    <div className='Box5'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>TRANSFEREE</h3>
+          </div>
+    </div>
+    <div className='Box6'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>2nd Course</h3>
+          </div>
+    </div>
+    <div className='Box7'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>2nd Course</h3>
+          </div>
+    </div>
+    <div className='Box8'>
+        <div className='text'>
+          <h1>996</h1>
+          <h3>RETURNEE</h3>
+          </div>
+    </div>
+    </div>
+  </div>
+    </>
+  );
+};
+  
+export default Dashboard;
